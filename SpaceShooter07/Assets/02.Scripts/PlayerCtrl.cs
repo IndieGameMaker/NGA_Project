38d86fa596adc844be8c5084e52df66bc30ddbb4
IndieGameMaker <myleje@gmail.com>
2019-07-27 15:05:04 +0900

    .

--- SpaceShooter07/Assets/02.Scripts/PlayerCtrl.cs
+++ SpaceShooter07/Assets/02.Scripts/PlayerCtrl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //주인공 캐릭터가 사용할 애니메이션 클립을 저장할 클래스 선언
+[System.Serializable]  //객체직렬화 어트리뷰트
 public class PlayerAnim
 {
     public AnimationClip idle;
@@ -10,6 +11,7 @@ public class PlayerAnim
     public AnimationClip runBackward;
     public AnimationClip runLeft;
     public AnimationClip runRight;
+    public AnimationClip[] dies;
 }
 
 public class PlayerCtrl : MonoBehaviour
@@ -26,6 +28,8 @@ public class PlayerCtrl : MonoBehaviour
         //PlayerCtrl 스크립트가 포함된 게임오브젝트의 Transform 컴퍼넌트를 추출해서 할당
         tr = GetComponent<Transform>();  //컴포넌트의 캐쉬
         anim = GetComponent<Animation>(); //Animation 컴포넌트 캐취 처리(변수에 할당)
+
+        anim.Play(playerAnim.idle.name); //"Idle" 반환
     }
 
     //매 프레임마다 호출 
