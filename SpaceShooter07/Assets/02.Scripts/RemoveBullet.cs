0b28f5b0d27be6d3f26f9d6d174f83ab6c40399a
IndieGameMaker <myleje@gmail.com>
2019-07-27 16:52:16 +0900

    .

---
+++ SpaceShooter07/Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    //충돌이 발생하면 1번 호출됨
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.tag == "BULLET")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
