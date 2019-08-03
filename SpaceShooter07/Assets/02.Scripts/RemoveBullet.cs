7348bf396701e2040a14dbd117faeb7ba98a3f34
IndieGameMaker <myleje@gmail.com>
2019-08-03 13:51:34 +0900

    ,

--- SpaceShooter07/Assets/02.Scripts/RemoveBullet.cs
+++ SpaceShooter07/Assets/02.Scripts/RemoveBullet.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
+
     //충돌이 발생하면 1번 호출됨
     void OnCollisionEnter(Collision coll)
     {
         if (coll.collider.tag == "BULLET")
         {
+            //충돌한 접점 정보를 추출
+            ContactPoint[] points = coll.contacts;
+            //스파크이펙트를 동적으로 생성  Instantiate(객체, 위치, 각도);
+            //Quaternion.LookRotation(벡터) --> 벡터의 각도를 쿼터니언 타입으로 변환(산출)
+            GameObject spark = Instantiate(sparkEffect
+                                        , points[0].point
+                                        , Quaternion.LookRotation(points[0].normal));
+            
+            //Destroy(삭제할 객체, 지연시간)
+            Destroy(spark, 0.6f);
+
             Destroy(coll.gameObject);
         }
     }
