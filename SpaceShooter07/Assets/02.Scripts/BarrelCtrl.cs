723570bdea33cbd5cd90092526c6a5e4ab9f2ee5
IndieGameMaker <myleje@gmail.com>
2019-08-03 15:37:37 +0900

    .

--- SpaceShooter07/Assets/02.Scripts/BarrelCtrl.cs
+++ SpaceShooter07/Assets/02.Scripts/BarrelCtrl.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class BarrelCtrl : MonoBehaviour
 {
-    
+    private int hitCount;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            if (++hitCount == 3)
+            {
+                ExpBarrel();
+            }
+        }
+    }
+
+    public void ExpBarrel()
+    {
+        if (GetComponent<Rigidbody>() != null)
+        {
+            Rigidbody rb = this.gameObject.AddComponent<Rigidbody>();
+            rb.AddForce(Vector3.up * 1500.0f);
+            Destroy(this.gameObject, 2.0f);
+        }
+    }
 }
