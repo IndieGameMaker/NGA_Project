0239312d66856cdf2cd0a6c2616ffa040221106e
IndieGameMaker <myleje@gmail.com>
2019-08-10 13:20:32 +0900

    .

--- SpaceShooter07/Assets/02.Scripts/MonsterCtrl.cs
+++ SpaceShooter07/Assets/02.Scripts/MonsterCtrl.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class MonsterCtrl : MonoBehaviour
 {
+    //몬스터의 상태
+    public enum State
+    {
+        IDLE,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+
+    //몬스터의 상태를 저장할 변수
+    public State state = State.IDLE;
+
+    public float attackDist = 2.0f;
+    public float traceDist  = 10.0f;
+    public bool isDie       = false;
+
     private Transform monsterTr;
     private Transform playerTr;
 
