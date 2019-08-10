199164638d2a9b57856854f43f11ad1912eba183
IndieGameMaker <myleje@gmail.com>
2019-08-10 13:15:37 +0900

    .

---
+++ SpaceShooter07/Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCtrl : MonoBehaviour
+{
+    private Transform monsterTr;
+    private Transform playerTr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterTr = GetComponent<Transform>();
+        playerTr  = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
