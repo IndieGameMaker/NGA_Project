52d882a8e2bc2ea0b1c32cb6904c7a4918639ece
IndieGameMaker <myleje@gmail.com>
2019-08-10 16:18:32 +0900

    .

--- SpaceShooter07/Assets/02.Scripts/FireCtrl.cs
+++ SpaceShooter07/Assets/02.Scripts/FireCtrl.cs
@@ -20,6 +20,7 @@ public class FireCtrl : MonoBehaviour
 
     void Update()
     {
+        Debug.DrawRay(firePos.position, firePos.forward * 10.0f, Color.green);
         //마우스 왼쪽 버튼을 한번 클릭했을 때
         if (Input.GetMouseButtonDown(0))
         {
@@ -30,7 +31,7 @@ public class FireCtrl : MonoBehaviour
     void Fire()
     {
         //동적 총알(Bullet)을 생성 (생성할 객체, 위치, 회전)
-        Instantiate(bullet, firePos.position, firePos.rotation);
+        //Instantiate(bullet, firePos.position, firePos.rotation);
         //총소리 발생 (소리를 중첩해서 발생)
         source.PlayOneShot(fireSfx);
         StartCoroutine(ShowMuzzleFlash());
